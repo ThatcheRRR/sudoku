cc0e6853f917c66a940c118d42aa0ddf9e9ab55d
ThatcheRRR <pavelosipov777@mail.ru>
2019-10-19 19:56:57 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    for (let col = 0; col < 9; col++) {
+        for (let row = 0; row < 9; row++) {
+            if (matrix[col][row] == 0) {
+                for (let num = 1; num < 10; num++) {
+                    if (checkValidation(matrix, col, row, num)) {
+                        matrix[col][row] = num;
+                        if (solveSudoku(matrix)) {
+                            return matrix;
+                        } else {
+                            matrix[col][row] = 0;
+                        }
+                    }
+                }
+            return false;
+            }
+        }
+    }
+    return matrix;
+}
+function checkValidation(board, row, col, num) {
+    for (let i = 0; i < 9; i++) {
+        let rowBox = Math.floor(row / 3) * 3 + Math.floor(i / 3);
+        let colBox = Math.floor(col / 3) * 3 + i % 3;
+        if (board[row][i] == num || board[i][col] == num || board[rowBox][colBox] == num) {
+            return false;
+        }
+    }
+    return true;
 }
